6e5a475b1916d22f6164ed1c8a071146bcd92046
ojogal <ojog.al@gmail.com>
2023-01-16 18:13:11 +0200

    Generate token method added

--- services/token_service.js
+++ services/token_service.js
@@ -1,6 +1,20 @@
+const jwt = require('jsonwebtoken');
+const tokenModel = require('../models/token.js');
+
 class TokenService {
-  generateToken() {
-    
+  generateToken(payload) {
+    const accessToken = jwt.sign(
+      payload, 
+      process.env.JWT_ACCESS_KEY, 
+      { expiresIn: '1h' });
+    const refreshToken = jwt.sign(
+      payload, 
+      process.env.JWT_REFRESH_KEY, 
+      { expiresIn: '10d' });
+    return {
+      accessToken,
+      refreshToken
+    }
   };
 };
 
