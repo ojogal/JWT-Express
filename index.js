96a4f2d19e0f878ae8b805eeb54780f37ddc494e
ojogal <ojog.al@gmail.com>
2023-01-16 12:17:40 +0200

    Server method created

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => { console.log(`Server sarted on port ${PORT}`) })
+  } catch (e) {
+    console.log(e)
+  }
+};
+
+start();
