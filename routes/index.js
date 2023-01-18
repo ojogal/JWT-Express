ee4a5623207f3df15bb4c8be4f4621b53e34d66a
ojogal <ojog.al@gmail.com>
2023-01-18 09:15:49 +0200

    Error handler created

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,15 @@ const { registration,
 const router = new Router();
 
 router.get('/users', index);
+
 router.get('/activate/:link', activate);
+
 router.get('/refresh', refresh);
+
 router.post('/registration', registration);
+
 router.post('/login', login);
+
 router.post('/logout', logout);
 
 module.exports = router
